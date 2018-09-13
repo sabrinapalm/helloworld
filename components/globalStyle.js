170b6a8559f5d6e4ade4dab0f90c981b6102b845
Johan Augustsson <j.augustsson80@gmail.com>
2018-09-13 17:07:53 +0200

    fix margins and server start

---
+++ components/globalStyle.js
@@ -0,0 +1,13 @@
+
+
+const GlobalStyle = ()=> (
+  <style jsx global>{`
+      body {
+         box-sizing: border-box;
+         margin: 0;
+         padding: 0;
+      }
+    `}</style>
+)
+
+export default GlobalStyle;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const next = require('next')
+const fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -8,6 +9,26 @@ const handle = app.getRequestHandler()
 app.prepare()
 .then(() => {
   const server = express()
+
+  server.get('/alltravels', (req, res) => {
+    let content;
+    const readFile = fs.readFile('./database.json', (err, data)=>{
+      if ( err ) throw err;
+      console.log(data);
+      console.log("funkar!", content);
+      content = data.toString();
+      res.send(content);
+    });
+
+
+  })
+
+
+
+
+
+
+
 
   server.get('*', (req, res) => {
     return handle(req, res)
