111028c0b545db66e8fdc74387fa27a301f4df72
Johan Augustsson <j.augustsson80@gmail.com>
2018-09-17 19:31:08 +0200

    update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.prepare()
 .then(() => {
   const server = express()
 
-  server.get('/alltravels', (req, res) => {
+  server.get('/travel/', (req, res) => {
     let content;
     const readFile = fs.readFile('./database.json', (err, data)=>{
       if ( err ) throw err;
@@ -19,16 +19,56 @@ app.prepare()
       content = data.toString();
       res.send(content);
     });
+  });
 
 
+  server.get('/travel/:id', (req,res) =>{
+    const id = req.params.id;
+    const readFile = fs.readFile('./database.json', (err, data)=>{
+      if ( err ) throw err;
+      let travels = JSON.parse(data.toString().trim());
+      if( travels[id] ) {
+        let responseObjet = data.toString().trim();
+        res.status(200).send(responseObjet)
+      } else {
+        res.status(200).send("finns ej")
+      }
+    })
   })
 
+  server.delete('/travel/:id', (req,res) =>{
+    const file = './database.json'
+    const id = req.params.id;
+    const readFile = fs.readFile(file, (err, data) => {
+      if ( err ) throw err;
+      let travels = JSON.parse(data.toString().trim());
+      if( travels[id] ) {
+        delete travels[id];
+        fs.writeFile(file, JSON.stringify(travels));
+        res.status(200).send("Borttagen!")
+      } else {
+        res.status(200).send("finns ej")
+      }
+    })
+  })
 
+  server.put('/travel/:id', (req,res) =>{
+    const file = './database.json'
+    console.log(req);
+    const id = req.params.id;
+    const { body } = req;
+    console.log("body is: ", req.body);
 
-
-
-
-
+    const readFile = fs.readFile(file, (err, data) => {
+      if ( err ) throw err;
+      let travels = JSON.parse(data.toString().trim());
+      if( travels[id] ) {
+        res.status(200).send("uppdaterad88!")
+      } else {
+        res.status(200).send("finns ej")
+      }
+    })
+  })
 
   server.get('*', (req, res) => {
     return handle(req, res)
